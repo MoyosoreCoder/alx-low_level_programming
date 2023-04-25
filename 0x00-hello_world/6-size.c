a3702f56372f08a6e541ff74af0ca8e69961a843
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-04-25 07:13:52 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include <sdtio.h>
 /**
-*main - A program that prints the various types of keywords
+*main - Entry point
 *Return: always 0 on success
 */
 int main(void)
 {
+	char;
+	int;
+	long int;
+	long long int;
+	float;
+	`
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long Int: %lu byte(s) \n", sizeof(long int));
-	printf("Size of a long long Int: %lu byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %lu  byte(s)\n", sizeof(float));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
-}
 
