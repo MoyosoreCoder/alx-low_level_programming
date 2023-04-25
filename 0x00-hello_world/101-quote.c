27546b0490bfcecde72c84534a69f894828a95a9
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-04-25 08:46:56 -0700

    6-size.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0 on success
+*/
+int main(void)
+{
+	char *message = "and that piece of art is useful" - Dora Korpar, 2015-10-19"\n;
+	write (STDERR_FILENO, message, 58);
+	return 1;
+}
