12ae5ea552fb022654929fb665ae3680bd43e38f
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-04-24 21:11:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*
+*main - Entry point of data
+*Return: always 0 on success
 */
-int main()
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-		return 0;
+		return (0);
 }
-
