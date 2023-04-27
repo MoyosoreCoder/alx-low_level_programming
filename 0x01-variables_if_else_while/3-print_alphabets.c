675ec7ce61e68b79ca881961bff9383ac21860ea
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-04-27 08:45:58 -0700

    ./3-print_alphabets | cat -e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of data
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
