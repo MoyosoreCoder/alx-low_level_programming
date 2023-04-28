20695fecaa433ce5dc354d210d12e2a7ba8d46ef
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-04-28 05:29:41 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of data
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
