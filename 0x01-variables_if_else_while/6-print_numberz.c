c5dc5bf0d58de0cabd014d383509f7ed8b5f0398
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-04-28 00:25:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of data
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n+ '0');
+
+	}
+	putchar('\n');
+	return (0);
+}
