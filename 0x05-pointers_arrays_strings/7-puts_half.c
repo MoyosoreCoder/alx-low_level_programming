b0dfb574e41375090c2f84e7dc360e36bdf396fb
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-05-20 16:42:17 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,27 +3,22 @@
 /**
  * puts_half - prints half of a string
  * @str: string to check before printing
- * Return: half of the string
+ * Return: half of the istring
  */
 void puts_half(char *str)
 {
 	int length = 0;
-	int i, start;
+	int i;
+	int n;
 
 	while (str[length] != '\0')
 	{
 		length++;
 	}
-	if (length % 2 == 0)
-	{
-		start = length / 2;
-	}
-	else
-	{
-		start = (length - 1) / 2;
-	}
 
-	for (i = start; i < length; i++)
+	n = (length + 1) / 2;
+
+	for (i = n; i < length; i++)
 	{
 		_putchar(str[i]);
 	}
