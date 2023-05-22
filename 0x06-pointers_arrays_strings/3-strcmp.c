f0875d4e2e64df4e0f72d9d0a5aaa7e7c534d3c1
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-05-22 03:45:31 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
