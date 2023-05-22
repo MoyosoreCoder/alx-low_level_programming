8fd117df586612a930217955ee810541ebb60dfa
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-05-22 13:22:45 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: pointer to an array consisting of string
+ * Return: always (0) on success
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
