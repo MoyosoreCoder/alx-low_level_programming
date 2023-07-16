61e69e5a5120b39d6a07b4e64200f2e8fe641a22
MoyosoreCoder <kelanimaryam1234@gnail.com>
2023-07-16 11:46:05 -0700

    0-malloc_checked.c updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
+ * malloc_checked - function that cause termination
+ * @b: the number of bytes to alloacte
+ * Return: exit value of 98
  */
 void *malloc_checked(unsigned int b)
 {
