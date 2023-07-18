64edf7dfdcd25d33e61979a3ece3651d94456a0b
MoyosoreCoder <kelanimaryam1234@gnail.com>
2023-07-18 09:27:47 -0700

    dog.h file added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - title or tag of structure
+ * @name: the name of te dog
+ * @age: age of the dog
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
