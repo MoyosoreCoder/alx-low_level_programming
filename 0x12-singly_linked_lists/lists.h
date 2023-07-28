d5b90974a2f6214932aa1c98be615da69687c790
MoyosoreCoder <kelanimaryam1234@gnail.com>
2023-07-28 16:06:22 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* struct list_s - singly linked list declaration
+* @str: string passed to function
+* @len: the length of the array of string
+* @next: pointer to next node
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
