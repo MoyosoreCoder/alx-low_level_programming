f023d4aaf0b7b48e5d556acc833bc144e7aac85a
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-09-04 03:56:47 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,26 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 /**
+ * main - the main entry
+ * @argc: the argument count
+ * @argv: the argument vector
  *
- *
+ * Return: the addition of digit,  return 1 if not is digit
  */
-int main(int argc, *char argv[])
+int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
+		printf("Error\n");
 	}
 	else if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if(!isdigit(*argv[i]))
+			if (!isdigit(*argv[i]))
 			{
 				printf("Error\n");
 				return (1);
