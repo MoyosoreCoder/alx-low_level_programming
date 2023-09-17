24f660ffaef0f7121bcc772996c64d4146b8755c
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-09-16 22:41:18 -0700

    0-sum_them_all.c committed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,8 +10,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i;
-	int sum = 0;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return (0);
@@ -20,9 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(args, int);
-	}
 	va_end(args);
 
 	return (sum);
