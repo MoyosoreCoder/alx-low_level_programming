734e24333c2fc6aa00b6e0d025de356fff88c566
MoyosoreCoder <kelanimaryam1234@gmail.com>
2023-09-21 04:50:54 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,37 +13,38 @@ void print_all(const char * const format, ...)
 	va_list args;
 	char *str;
 	int i = 0;
+	char *sep = "";
 
 	va_start(args, format);
 
-	while (format && format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			break;
-		default:
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
 			i++;
-			continue;
 		}
-		if (format[i + 1] != '\0')
-			printf(", ");
-		i++;
 	}
 	printf("\n");
 	va_end(args);
