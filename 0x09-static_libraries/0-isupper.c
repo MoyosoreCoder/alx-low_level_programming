d4987ce4881e0b5f42951f01b09d0e9f2add3af9
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-02 20:30:45 -0700

    static library object codes

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - function that check if upper case
+ * @c: character to check
+ * Return: 1 if c is upper, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
