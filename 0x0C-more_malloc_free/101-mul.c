d7129e3ccba191b4d30ea5cd54a44a85c121f207
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-10 19:36:43 -0700

    deleted new

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _strlen(char *str)
+{
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ *
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	char *num1 = arg[1];
+	char *num2 = arg[2];
+
+	for (i = 0; i < 
+}	
