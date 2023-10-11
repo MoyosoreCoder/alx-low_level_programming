ba462fb8d61f543d0af82cdc82c05ca7bc3d9c32
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-10 17:47:42 -0700

    commit message dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct declaration
+ * @name: the name in the structure
+ * @age: the age
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ *dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
