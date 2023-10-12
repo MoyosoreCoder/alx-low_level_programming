e250a60a464bb0c2a4188ed5cfeddb8ee4cdf85f
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-11 23:05:26 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,11 @@
 #include "3-calc.h"
-
+#include <stdio.h>
+/**
+ * get_op_func - selects the correct function to parameter
+ * @s: the pointer that point to a character array
+ *
+ * Return: the calculation according to the symbol between the two numbers
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -15,11 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+	return (ops[i].f);
 }
