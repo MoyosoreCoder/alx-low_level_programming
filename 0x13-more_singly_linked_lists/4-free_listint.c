342e22d0ac42dfda00256485c260737bd7fa3f22
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-25 07:57:27 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,10 +7,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	 while (head)
-	 {
-		 temp = head->next;
-		 free(head);
-		 head = temp;
-	 }
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
+
