1c5e000cd81ca6e403b41fbc207380f718a25215
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-26 20:27:37 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,7 @@ unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int base, unsigned int power);
-
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
