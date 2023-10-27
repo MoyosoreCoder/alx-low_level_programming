f50db082c0aebebbafdc9199b3e7792db93ac1c5
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-10-26 20:15:46 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,5 +3,7 @@
 
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+void print_binary(unsigned long int n);
+unsigned long int _pow(unsigned int base, unsigned int power);
 
 #endif
