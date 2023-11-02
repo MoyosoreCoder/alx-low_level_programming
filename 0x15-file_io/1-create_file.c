11d68d15ca792ebfeaaa8c46a7e869b3dba94bd5
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-11-01 20:28:49 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,17 +16,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_byte = write(fd, text_content, len);
 
-	if (write_byte == -1)
+	if (write_byte == -1 || fd == -1)
 		return (-1);
 	close(fd);
 
