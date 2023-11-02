d0219fc863b39a85f99c78aaa86a4430e5d1a693
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-11-01 23:43:55 -0700

    2-append_text_to_file.c updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	write_byte = write(fd, text_content, len);
 
-	if(fd == -1 || write_byte == -1)
+	if (fd == -1 || write_byte == -1)
 		return (-1);
 	close(fd);
 
