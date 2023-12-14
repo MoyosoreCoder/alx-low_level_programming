6a1ae08f3fd951f747bcefd36934289c1fa53edc
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-12-14 04:14:49 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
+ * print_dlistint - a function that print element in a node
+ * @h: pointer to the head or first node in a list
  *
- *
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
