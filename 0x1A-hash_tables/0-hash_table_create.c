16cfff7484fc63d3b1b5fdb2fdf34a6d0636f20d
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-12-25 18:31:49 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_table_t *table;
 
 	if (size == 0)
 		return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	table = calloc(1, sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (hash_table->array == NULL)
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
-		free(hash_table);
+		free(table);
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	hash_table->size = size;
-	return (hash_table);
+	return (table);
 }
