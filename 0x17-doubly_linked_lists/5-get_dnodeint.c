54fd9d8827fdf83d4f7d3f63f409b84cfeb7f882
MoyosoreCoder <kelanimaryam1234@gail.com>
2023-12-31 04:58:55 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,7 +2,7 @@
 /**
  * get_dnodeint_at_index - a function that returns the nth node of a dlistint_t
  * @head: a pointer to a dlistint_t linked list
- * @index: index to return the data at the end
+ * @index: index to return the data at the end of the list
  * Return: the node does not exist, return NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
